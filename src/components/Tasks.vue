da043986dff8485880d9b274e790303eeb3f157d
kaaviya_n <kaaviya.nakkeeran@outlook.com>
2021-04-03 23:57:55 +0530

    Task Tracker - Vue

---
+++ src/components/Tasks.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div :key="task.id" v-for="task in tasks">
+      <Task
+        @reminder-click="$emit('reminder-click', task.id)"
+        @delete-task="$emit('delete-task', task.id)"
+        :task="task"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Task from "./Task";
+
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+  emits: ["delete-task", "reminder-click"],
+};
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h2>New to VueJs : April 2020</h2>
+    <h3>Version 1.0.0</h3>
+    <router-link to="/">Go back</router-link>
+  </div>
+</template>
